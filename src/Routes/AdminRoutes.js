ddc3b34d20374c96cbc4ce63f5da6d4ac66b72c6
princychodvadiya <princychodvadiya721@gmail.com>
2024-03-19 19:42:23 +0530

    19-3

--- src/Routes/AdminRoutes.js
+++ src/Routes/AdminRoutes.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom/dist';
 import Products from '../admin/container/Products/Products';
+import Reviews from '../admin/container/Reviews/Reviews';
 import PrivateRoutes from './PrivateRoutes';
+import Layout from '../admin/component/Layout/Layout';
+
 
 function AdminRoutes(props) {
     return (
-        <>
+        <Layout>
             <Routes>
                 <Route element={<PrivateRoutes />}>
                     <Route excat path='/products' element={<Products />} />
+                    <Route excat path='/Reviews' element={<Reviews />} />
                 </Route>
             </Routes>
-        </>
+        </Layout>
     );
 }
 
